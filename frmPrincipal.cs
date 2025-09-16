5e9eeff7b865c29353d62b6b7b383973a58e673e
lukitaz-programador <lucasromo.personal@gmail.com>
2025-09-16 14:30:24 -0300

    Exporatación de vectores

--- frmPrincipal.cs
+++ frmPrincipal.cs
@@ -1,14 +1,58 @@
 namespace pryRomoGestionAgendaContactos;
 public partial class frmPrincipal : Form
 {
+    public static string[] vecExportado=new string[5];
+
     public frmPrincipal()
     {
         InitializeComponent();
     }
 
-    string vTelefono;
-    string vNombre;
-    int vContador=0;
+    //Definción de variables globales
+    string[] vecTelefono = new string[5];
+    string[] vecContacto = new string[5];
+    int vContador = 0;
+    int indice;
+
+    //Campos
+    private void txtContacto_TextChanged(object sender, EventArgs e)
+    {
+        if (txtContacto.Text == "")
+        {
+            mtbNumero.Enabled = false;
+        }
+        else
+        {
+            mtbNumero.Enabled = true;
+        }
+    }
+
+    private void mtbNumero_TextChanged(object sender, EventArgs e)
+    {
+        if (mtbNumero.MaskFull == true)
+        {
+            btnRegistrar.Enabled = true;
+        }
+        else
+        {
+            btnRegistrar.Enabled = false;
+        }
+    }
+
+    //Botones
+    private void btnRegistrar_Click(object sender, EventArgs e)
+    {
+        vecContacto[indice] = txtContacto.Text;
+        vecTelefono[indice] = mtbNumero.Text;
+        lsbRegistros.Items.Add("Contacto: " + vecContacto[indice] + " Teléfono: " + vecTelefono[indice]);
+        vContador++;
+        lblContador.Text = Convert.ToString(vContador);
+        txtContacto.Text = "";
+        mtbNumero.Text = "";
+        vecExportado[indice]=vecContacto[indice]+vecTelefono[indice];
+        indice++;
+        txtContacto.Focus();
+    }
 
     private void btnCancelar_Click(object sender, EventArgs e)
     {
@@ -16,38 +60,15 @@ public partial class frmPrincipal : Form
         mtbNumero.Text = "";
     }
 
-    private void txtContacto_TextChanged(object sender, EventArgs e)
+    private void lsbRegistros_SelectedIndexChanged(object sender, EventArgs e)
     {
-        if (txtContacto.Text == "")
-        {
-            mtbNumero.Enabled = true;
-        }
-        else
-        {
-            mtbNumero.Enabled = false;
-        }
+
     }
 
-    private void mtbNumero_KeyDown(object sender, KeyEventArgs e)
+    private void button1_Click(object sender, EventArgs e)
     {
-        if (mtbNumero.MaskFull)
-        {
-            btnRegistrar.Enabled = false;
-        }
-        else
-        {
-            btnRegistrar.Enabled = true;
-        }
-    }
-
-    private void btnRegistrar_Click(object sender, EventArgs e)
-    {
-        vNombre = txtContacto.Text;
-        vTelefono = mtbNumero.Text;
-        lsbRegistros.Items.Add("Contacto: " + vNombre + " Teléfono: " + vTelefono);
-        vContador = vContador + 1;
-        lblContador.Text =Convert.ToString(vContador);
-        txtContacto.Text = "";
-        mtbNumero.Text = "";
+        frmMuestra frmMuestra= new frmMuestra();
+        Hide();
+        frmMuestra.ShowDialog();
     }
 }
